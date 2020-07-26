dc075c0bda76d020f4da24e857d65bb5768d23b4
LughWalle <civalfran1@gmail.com>
2020-07-26 17:40:01 -0500

    fazendo funcao de troca cor

--- 5_4/script.js
+++ 5_4/script.js
@@ -1,6 +1,16 @@
 let body = document.body;
+/* Cria div de style */
+body.onload = adcDivStyle;
+function adcDivStyle() {
+  alert("Ola mundo!")
+}
+
+/*Funcoes navBar*/
+let navBar = body.firstElementChild.querySelector(".nav-style");
+
+// mostra/esconde div
 let show = false;
-let navBar = body.firstElementChild.firstElementChild.querySelector(".nav-style");
+navBar.style.display = 'none';
 function navStyle() {
   if (!show && navBar.style.display === 'none') {
     show = true;
@@ -16,3 +26,23 @@ function navStyle() {
   
 }
 body.firstElementChild.firstElementChild.querySelector('#bar-style').addEventListener('click', navStyle);
+
+// muda cores
+let aUm = navBar.querySelector('a')[0];
+let color1 = 'rgba(45, 150, 196, 50)';
+let color2 = 'rgba(20, 70, 196, 25)';
+let change = false;
+function changeColor() {
+  if (!change && aUm.style.backgroundColor === color1) {
+    console.log(change = true);
+  }
+  if(change && aUm.style.backgroundColor === color2) {
+    console.log(change = false);
+  }
+  // if (!change) {
+  //   aUm.style.backgroundColor = color2;
+  // } else {
+  //   aUm.style.backgroundColor = color1;
+  // }
+}
+navBar.firstElementChild.addEventListener('click', changeColor);
