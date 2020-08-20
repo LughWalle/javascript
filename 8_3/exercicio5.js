28d6658cb39e487526f795a23fc2f2b22ef32e7d
LughWalle <civalfran1@gmail.com>
2020-08-20 16:23:50 -0500

    exercicio.js

---
+++ 8_3/exercicio5.js
@@ -0,0 +1,17 @@
+    
+const assert = require('assert');
+
+const names = [
+  'Aanemarie',  'Adervandes',   'Akifusa',
+  'Abegildo',   'Adicellia',    'Aladonata',
+  'Abeladerco', 'Adieidy',  'Alarucha',
+];
+
+
+
+function containsA() {
+
+  return names.reduce((acc, cur) => acc.split(''))
+}
+
+assert.deepEqual(containsA(), 20);
---
+++ exercicio.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+// implemente seus testes aqui
+assert.strictEqual(sum(4, 5), 9, 'esperava 9 e retornou outro numero');
+assert.strictEqual(sum(0, 0), 0, 'errado');
+assert.strictEqual(sum(4, '5'), 9);
+
+// exercise 2
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+
